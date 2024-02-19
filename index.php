20ac636f0b0625cb82c433a96553758f7577a3b1
Reinaldo Ferreira <reinaldojuly@gmail.com>
2024-02-19 16:48:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="/estilos/global.css">
         <title>VL56 Consultorias</title>
+
     </head>
 
     <body>
              <!--adicione aqui um comentario-->
         <div class="container">
 
-                    <nav>
+                    <nav class="menu" id="menu-inicial">
                         
                              
                             <ul>
                                 <li><a href="index.html">Sobre mim</a></li>
-                                <li><a href="#">Parceiros e Projetos</a></li>
+                                <li><a href="projetos.html">Parceiros e Projetos</a></li>
                                 <li><a href="contato.html">Contato</a></li>
             
                             </ul>
@@ -31,40 +30,30 @@
     
                     <header>
 
-    
-        
                             <div class="center">
                                  <img src="img/logo01-removebg-preview.png" alt="img-logo-fundo">
                             </div>
-                                <!--<dd>adicione aqui um comentario
-                                    <h1>Reinaldo Ferreira</h1>
-                                    <h2>Desenvolvedor e Consultor de ISP</h2>
-                                </dd>-->
+
                     </header>
     
 
     
             <main>
-                <section>
+                <section class="texto">
 
-                    <dd><h3> Sobre mim </h3><!--Biografia da empresa ou pessoa-->
+                    <h3> Sobre mim </h3><!--Biografia da empresa ou pessoa-->
                 
                         <p>
-                            Desde 2018, atuo com consultoria oferecendo as melhores soluções no mercado, aliando conhecimento e técnica e 
-                            utilizando da melhor maneira possível os recursos disponíveis para provedores de internet.
-
-                            Nossa missão é utilizar a expertise de nosso time para elevar a qualidade dos serviços oferecidos no mercado, ajudando a conectar ideias e pessoas, 
-                            buscando constante desenvolvimento e aperfeiçoamento.
-
-                            Desta forma, construímos nossa história com redes de relacionamento baseado em resultado e confiança, 
-                            crescendo cada vez mais solidificados na qualidade de nosso trabalho e dedicação.
+                            Desde 2018, atuamos com consultoria oferecendo as melhores soluções no mercado, aliando conhecimento e técnica e utilizando da melhor maneira possíveis os recursos disponíveis para provedores de internet.
+                            Nossa missão é utilizar a experiência de nosso time para elevar a qualidade dos serviços oferecidos no mercado, ajudando a conectar ideias e pessoas, buscando constante desenvolvimento e aperfeiçoamento. 
+                            Desta forma, construímos nossa história com redes de relacionamento baseado em resultado e confiança, crescendo cada vez mais solidificados na qualidade de nosso trabalho e dedicação.
                         </p>
-                    </dd>    
+                        
                 </section>
 
-                <section>
-                    <dd><!--adicione aqui um comentario-->
-                        <h3>Experiências com </h3>
+                <section class="slider-servicos" id="slide-img">
+                    <!--adicione aqui um comentario-->
+                        <h3>Experiências com:</h3>
 
                             <div class="mk">
 
@@ -72,58 +61,46 @@
                                     
                                     <div class="slider-container">
                                         <div class="slider">
-                                          <div class="slide"><img src="img/switch/cisco/WS-C2960-48TC-S-02.png" alt="Imagem 1"><dd>C2960</dd></div>
-                                          <div class="slide"><img src="img/switch/datacom/dm-1200-datacom-removebg.png" alt="Imagem 2"><dd>DM-1200</dd></div>
-                                          <div class="slide"><img src="img/router/huawei/ne_8000_m8_dc_01_1__1_-removebg-preview.png" alt="Imagem 3"><dd>NE8000</dd></div>
-                                          <div class="slide"><img src="img/router/mikrotik/ccr2116-removebg-preview.png" alt="Imagem 4"><dd>CCR 2116</dd></div>
-                                          <div class="slide"><img src="img/router/mikrotik/rb3011-removebg-preview.png" alt="Imagem 5"><dd>RB 3011</dd></div>
-                                          <div class="slide"><img src="img/olt/huawei/ma5800-x2-removebg-preview.png" alt="Imagem 6"><dd>MA5800</dd></div>
+                                          <div class="slide"><img src="img/switch/cisco/WS-C2960-48TC-S-02.png" alt="Imagem 1"></div>
+                                          <div class="slide"><img src="img/switch/datacom/dm-1200-datacom-removebg.png" alt="Imagem 2"></div>
+                                          <div class="slide"><img src="img/router/huawei/ne_8000_m8_dc_01_1__1_-removebg-preview.png" alt="Imagem 3"></div>
+                                          <div class="slide"><img src="img/router/mikrotik/ccr2116-removebg-preview.png" alt="Imagem 4"></div>
+                                          <div class="slide"><img src="img/router/mikrotik/rb3011-removebg-preview.png" alt="Imagem 5"></div>
+                                          <div class="slide"><img src="img/olt/huawei/ma5800-x2-removebg-preview.png" alt="Imagem 6"></div>
                                           <!-- Adicione mais slides conforme necessário -->
                                         </div>
                                     </div>
                                 </li>
                              </div>
 
-                             
-
-                        <div class="mk02"><!--SEPARA MK02 DE SLIDE-->
-                            <dd>
-                                <li>Desenvolvedor</li>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                            </dd> 
-                        </div>
-                    
-                    </dd>
                 </section>
         
-                
-
-
-
             </main>
     
+            
         </div>
 
         <footer>
             <div class="footer-item">
-                <a href="https://www.instagram.com/seu_usuario" target="_blank" rel="noopener noreferrer">
-                <i class="fa fa-instagram"></i>Instagram</a>
+            <a href="https://www.instagram.com/seu_usuario" target="_blank" rel="noopener noreferrer">
+              <i class="fa fa-instagram"></i>Instagram</a>
             </div>
           
             <div class="footer-item">
-                <a href="https://www.facebook.com/seu_usuario" target="_blank" rel="noopener noreferrer">
-                  <i class="fa fa-facebook"></i>Facebook</a>
+            <a href="https://www.facebook.com/seu_usuario" target="_blank" rel="noopener noreferrer">
+              <i class="fa fa-facebook"></i>Facebook</a>
+            
+          
             </div>
           
             <div class="footer-item">
-                  <a href="https://www.linkedin.com/in/seu_usuario" target="_blank" rel="noopener noreferrer">
-                  <i class="fa fa-linkedin"></i>Linkdin</a>
+              <a href="https://www.linkedin.com/in/seu_usuario" target="_blank" rel="noopener noreferrer">
+              <i class="fa fa-linkedin"></i>Linkdin</a>
             </div>
           
             <div class="footer-item">
-                <a href="https://github.com/seu_usuario" target="_blank" rel="noopener noreferrer">
-                  <i class="fa fa-github"></i>Github</a>
+            <a href="https://github.com/seu_usuario" target="_blank" rel="noopener noreferrer">
+              <i class="fa fa-github"></i>Github</a>
             </div>
           </footer>
 
