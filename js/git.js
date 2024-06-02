376c3dad4673c321addc3fd086121be2b794331d
Reinaldo Ferreira <reinaldojuly@gmail.com>
2024-06-02 16:54:55 -0300

    update

---
+++ js/git.js
@@ -0,0 +1,10 @@
+
+function onLampada() {
+    document.getElementById("githube").src = "/img/_img_sobre_/on.png"
+
+}
+
+function offLampada() {
+    document.getElementById("github").src = "/img/_img_sobre_/off.png"
+
+}
---
+++ js/tube.js
@@ -0,0 +1,10 @@
+
+function onLampada() {
+    document.getElementById("tube").src = "/img/_img_sobre_/on.png"
+
+}
+
+function offLampada() {
+    document.getElementById("tube").src = "/img/_img_sobre_/off.png"
+
+}
