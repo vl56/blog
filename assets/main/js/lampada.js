605b07f6ec5f3f5ebf38ab2c42d899c0c0098ea5
Reinaldo Ferreira <reinaldojuly@gmail.com>
2024-06-02 11:35:11 -0300

    update

--- assets/main/js/lampada.js
+++
@@ -1,9 +0,0 @@
-function onLampada() {
-    document.getElementById("luz").src = "assets/main/img/_img_sobre_/on.png"
-
-}
-
-function offLampada() {
-    document.getElementById("luz").src = "assets/main/img/_img_sobre_/off.png"
-
-}
---
+++ js/lampada.js
@@ -0,0 +1,9 @@
+function onLampada() {
+    document.getElementById("luz").src = "/assets/main/img/_img_sobre_/on.png"
+
+}
+
+function offLampada() {
+    document.getElementById("luz").src = "/assets/main/img/_img_sobre_/off.png"
+
+}
