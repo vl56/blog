aab0fe3ba498dbffa2b3513fabe367b5251a103b
Reinaldo Ferreira <reinaldojuly@gmail.com>
2024-06-02 15:34:41 -0300

    Update lampada.js

--- js/lampada.js
+++ js/lampada.js
@@ -1,9 +1,9 @@
 function onLampada() {
-    document.getElementById("luz").src = "/assets/main/img/_img_sobre_/on.png"
+    document.getElementById("luz").src = "/img/_img_sobre_/on.png"
 
 }
 
 function offLampada() {
-    document.getElementById("luz").src = "/assets/main/img/_img_sobre_/off.png"
+    document.getElementById("luz").src = "/img/_img_sobre_/off.png"
 
 }
