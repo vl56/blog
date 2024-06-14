abd98ce0221bc7266cff99819838fc3e0e15114e
Reinaldo Ferreira <reinaldojuly@gmail.com>
2024-06-14 11:00:58 -0300

    update

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ app = Flask(__name__)
 
 # Router - local onde esta as paginas
 @app.route('/')
-@app.route('/db.php')
-@app.route('/cadastro.php')
-@app.route('/envia.php')
 
 #Função - o que deseja ser exibido na pagina
 def index():
     return render_template('index.php')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="localhost",port="5588", debug=True)
